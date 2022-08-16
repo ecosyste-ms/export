1a6b97799c7b2ac6e14ff7f98a7b2f656e98123e
Andrew Nesbitt <andrewnez@gmail.com>
2022-08-16 15:49:13 +0100

    bucket name argument

--- export.sh
+++ export.sh
@@ -3,6 +3,10 @@
 # get service name
 service_name=$1
 
+default_bucket_name=ecosystems-data
+
+bucket_name="${2:-$default_bucket_name}"
+
 database="$service_name"_production
 
 # get current date
@@ -19,16 +23,16 @@ mkdir $folder
 dokku postgres:export $database > $folder/$service_name-$date.sql
 
 # download license into folder
-wget -O $folder/license.txt https://creativecommons.org/licenses/by-sa/4.0/legalcode.txt
+wget -O $folder/license.txt https://raw.githubusercontent.com/ecosyste-ms/export/main/export-license.txt
 
 # download readme into folder
-wget -O $folder/readme.md https://raw.githubusercontent.com/ecosyste-ms/export/master/README.md
+wget -O $folder/readme.md https://raw.githubusercontent.com/ecosyste-ms/export/main/export-readme.md
 
 # compress folder 
 tar -czf $folder.tar.gz -C /tmp/ $path
 
 # upload to s3
-aws s3 cp $folder.tar.gz s3://ecosystems-data/
+aws s3 cp $folder.tar.gz s3://$bucket_name/
 
 # trigger rake task in $service_name service to record export
 # dokku run --env EXPORT_DATE=$date $service_name rake exports:record
